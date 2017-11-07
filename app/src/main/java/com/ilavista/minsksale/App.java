0449b550c44f09f4cd36d3b240efe1f17e49089a
Yauheni Farmakidau <yauheni.farmakidau@itechart-group.com>
2017-11-07 23:48:49 +0300

    Fabric integration

---
+++ app/src/main/java/com/ilavista/minsksale/App.java
@@ -0,0 +1,23 @@
+package com.ilavista.minsksale;
+
+import android.app.Application;
+
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
+import timber.log.Timber;
+
+public class App extends Application {
+    private static final String RELEASE_BUILD_TYPE = "release";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (RELEASE_BUILD_TYPE.equals(BuildConfig.BUILD_TYPE)) {
+            Fabric.with(this, new Crashlytics());
+        } else {
+            Timber.plant(new Timber.DebugTree());
+            Fabric.with(this);
+        }
+    }
+}
