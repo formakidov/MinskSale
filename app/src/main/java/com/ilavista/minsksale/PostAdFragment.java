0a568a1b8ffb956fe21f6c0a617095738619fa2c
Yauheni Farmakidau <yauheni.farmakidau@itechart-group.com>
2017-11-07 23:09:42 +0300

    Change mail link

--- app/src/main/java/com/ilavista/minsksale/PostAdFragment.java
+++ app/src/main/java/com/ilavista/minsksale/PostAdFragment.java
@@ -3,17 +3,21 @@ package com.ilavista.minsksale;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class PostAdFragment extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_post_ad, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_post_ad, container, false);
+        TextView mail = view.findViewById(R.id.mailTv);
+        mail.setMovementMethod(LinkMovementMethod.getInstance());
+        // TODO: 11/7/17 кнопку "написать нам" ?
+        return view;
     }
 
 }
